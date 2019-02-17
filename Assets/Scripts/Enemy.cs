b7c7bb516c3093f64ca85cc09237c51cf9ba5164
zeFerret <justinas.seskevicius@gmail.com>
2019-02-17 13:31:02 +0200

    feat: add score & self-destruction for explosion VFX

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] int points = 100;
+
+    Score score;
+
+    private void Start()
+    {
+        score = FindObjectOfType<Score>();
+    }
+
     private void OnParticleCollision(GameObject other)
     {
+        score.AddScore(points);
+        Instantiate(deathVFX, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    Text scoreText;
+    int score = 0;
+
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        UpdateScore();
+    }
+
+    private void UpdateScore()
+    {
+        scoreText.text = score.ToString();
+    }
+
+    public void AddScore(int amount)
+    {
+        score += amount;
+    }
+}
